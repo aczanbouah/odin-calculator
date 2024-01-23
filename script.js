b8aeff312dd863a7ab07987aacf8f1420f681432
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-01-23 23:18:47 +0200

    Add operate function and helper variables

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
 function add(num1, num2) {
   return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2) {
-  return parseInt(num1) + parseInt(num2);
+  return parseInt(num1) - parseInt(num2);
 }
 
 function multiply(num1, num2) {
@@ -12,4 +16,17 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return parseInt(num1) / parseInt(num2);
+}
+
+function operate(num1, op, num2) {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
 }
