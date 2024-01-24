32e2a2da84cec3a4a0b4f6ea0c8d3d39479ac363
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-01-24 23:59:01 +0200

    Add functionality to populate the display when numbers are clicked

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let firstNumber;
 let operator;
 let secondNumber;
+let displayValue = 0;
+
+const numberValue = document.querySelector(".display-value");
+updateDisplay();
+
+const allNumberButtons = document.querySelectorAll(".number");
+allNumberButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    displayValue += e.target.dataset.value;
+    updateDisplay();
+  });
+});
 
 function add(num1, num2) {
   return parseInt(num1) + parseInt(num2);
@@ -35,4 +47,8 @@ function operate(num1, op, num2) {
     case "%":
       return percentage(num1, num2);
   }
+}
+
+function updateDisplay() {
+  numberValue.innerText = parseInt(displayValue);
 }
