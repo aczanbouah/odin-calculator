7473d67b470acc2fe74f7a08462ee36f808aeacd
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-01-26 17:25:26 +0200

    Add clear and power functions and styling adjusments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let firstNumber;
-let operator;
-let secondNumber;
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
 let displayValue = 0;
 
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clear);
 const numberValue = document.querySelector(".display-value");
 updateDisplay();
 
@@ -34,6 +36,10 @@ function percentage(num1, num2) {
   return ((parseInt(num1) / parseInt(num2)) * 100).toFixed(2);
 }
 
+function power(num1, num2) {
+  return parseInt(num1) ^ parseInt(num2);
+}
+
 function operate(num1, op, num2) {
   switch (op) {
     case "+":
@@ -46,9 +52,19 @@ function operate(num1, op, num2) {
       return divide(num1, num2);
     case "%":
       return percentage(num1, num2);
+    case "^":
+      return power(num1, num2);
   }
 }
 
 function updateDisplay() {
   numberValue.innerText = parseInt(displayValue);
+}
+
+function clear() {
+  firstNumber = 0;
+  operator = "";
+  secondNumber = 0;
+  displayValue = 0;
+  updateDisplay();
 }
